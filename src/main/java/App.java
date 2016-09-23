ad839e0a0938522272fbc9cf38f1cf27ee60e1e0
Shelby Clayton <shelby_clayton@hotmail.com>
2016-09-23 11:46:07 -0700

    create create a page that list the individual stats of a stylist with client list. Corresponding route on App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -81,6 +81,16 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/stylists/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+      List<Client> client = stylist.getClients();
+      model.put("client", client);
+      model.put("stylist", stylist);
+      model.put("template", "templates/stylist.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
 
   }
 }
